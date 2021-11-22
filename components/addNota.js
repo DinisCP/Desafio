6331b834d734b20661050c67921b520dbe48ace3
Dinis <94561516+DinisCP@users.noreply.github.com>
2021-11-22 17:19:59 +0000

    Add files via upload

---
+++ components/addNota.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
+
+export default function AddNota({ submitHandler }) {
+  [text, setText] = useState('');
+
+  const changeHandler = (val) => {
+    setText(val);
+  };
+
+  return (
+    <View>
+      <TextInput 
+        style={styles.input} 
+        placeholder='nova nota...'
+        onChangeText={changeHandler} 
+        value={text} 
+      />
+      <Button color='#6D28D9' onPress={() => submitHandler(text)} title='adicionar nota' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    marginBottom: 10,
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderBottomWidth: 1,
+    borderBottomColor: '#6D28D9',
+  },
+});
